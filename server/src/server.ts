0b4266ca48ab854bc93af52ebab9c902258b7c03
Monteifa <fabiopmont@gmail.com>
2020-10-20 09:47:55 -0300

    Server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  return response.send('oi');
+});
+
 app.listen(3333);
